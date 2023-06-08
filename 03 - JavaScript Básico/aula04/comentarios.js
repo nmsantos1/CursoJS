f617b3905bd75df46ac659af328e2e0a05fc00a4
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-08 12:08:26 -0300

    aula04 - //comentarios

---
+++ 03 - JavaScript Básico/aula04/comentarios.js
@@ -0,0 +1,10 @@
+//Comentário
+console.log('Olá Mundo!'); //Comentário
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+//Comentário
+
+/*Comentário longo
+bloco de comentário*/
