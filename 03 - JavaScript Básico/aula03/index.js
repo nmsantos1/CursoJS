ca59675a585c1520c4e5814962f947267652321f
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-08 11:51:35 -0300

    console.log

---
+++ 03 - JavaScript Básico/aula03/index.js
@@ -0,0 +1,4 @@
+console.log('Natanael Marconato'); // String
+console.log("Natanael Marconato"); // String
+console.log(`Natanael Marconato`); // String
+console.log(123, 15.06);           // Number
