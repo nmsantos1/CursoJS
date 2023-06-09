3d085254f62ea9c09debf08b6106e10029c3d7e8
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-09 15:35:52 -0300

    aula06 - Variáveis com let

---
+++ 03 - JavaScript Básico/aula06/index.js
@@ -0,0 +1,16 @@
+/*let nomeJoao = 'João';
+
+console.log(nomeJoao, 'nasceu em 1984.');
+console.log('Em 2000', nomeJoao, 'conheceu Maria.');
+console.log(nomeJoao, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nomeJoao, 'em 2015.');
+console.log('O filho de', nomeJoao, 'se chama Eduardo.');*/
+
+// não usar VAR, usar LET
+
+let nome; //declarando variável sem inicializar
+nome = 'João'; //inicializando variável
+console.log(nome); 
+nome = 'Natanael';
+console.log(nome);
+
