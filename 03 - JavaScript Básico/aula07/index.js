1d1a052cb39db334f81809f5f8a4b06de98d0f67
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-10 14:40:24 -0300

    aula07 - Constantes com const

---
+++ 03 - JavaScript Básico/aula07/index.js
@@ -0,0 +1,4 @@
+const primeiroNum = 5; //é preciso inicializar já na criação da constante, seu valor não é modificavel
+const segundoNum = 10;
+const resultado = primeiroNum * segundoNum;
+const resultadoDuplicado = resultado * 2;
