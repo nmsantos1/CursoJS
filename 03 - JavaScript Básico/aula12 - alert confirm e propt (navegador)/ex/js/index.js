34f1dbba6f10428a0795b259871ba84ce317f7fc
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-12 18:47:05 -0300

    aula12/13

---
+++ 03 - JavaScript Básico/aula12 - alert confirm e propt (navegador)/ex/js/index.js
@@ -0,0 +1,5 @@
+let num1 = Number(prompt('Digite um número:'));
+let num2 = Number(prompt('Digite outro número:'));
+let result = num1 + num2;
+
+alert(`${num1} + ${num2} = ${result}`);
---
+++ 03 - JavaScript Básico/aula12 - alert confirm e propt (navegador)/js/index.js
@@ -0,0 +1,3 @@
+alert('Mensagem.'); //undefined
+let confirma = confirm('Deseja apagar?'); //true or false
+let num1 = prompt('Digite um numero:'); //retorna o que foi digitado em string
