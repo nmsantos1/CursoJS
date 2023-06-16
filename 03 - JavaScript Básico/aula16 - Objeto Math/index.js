097c2f43d7c3c5de9f9d3e270a49ec5656de85a2
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-06-16 18:28:43 -0300

    aula16 - Objeto Math

---
+++ 03 - JavaScript Básico/aula16 - Objeto Math/index.js
@@ -0,0 +1,17 @@
+let num1 = 9.54578;
+console.log(num1 ** (1/2)); // ==> raiz quadrada | num1 ** 0.5
+
+//let num2 = Math.floor(num1); #arredonda para baixo
+//let num2 = Math.ceil(num1); #arredonda para cima
+//let num2 = Math.round(num1); #arredonda para o mais próximo
+
+console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 10)); // ==> 1500 (maior numero)
+console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 10)); // ==> -50  (menor numero)
+console.log(Math.random()); // gera um numero aleatório entre 0 e 1
+
+const aleatorio = (Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
+
+console.log(Math.PI); //  valor de PI
+
+console.log(Math.pow(2, 10)); // = 2 ** 10 = 2 ^ 10
