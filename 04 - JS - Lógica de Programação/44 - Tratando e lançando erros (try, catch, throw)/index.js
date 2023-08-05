f3a3c09bea1841146c255be97fba7a19249f1114
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-08-05 15:36:44 -0300

    44 - Tratando e lançando erros (try, catch, throw)

---
+++ 04 - JS - Lógica de Programação/44 - Tratando e lançando erros (try, catch, throw)/index.js
@@ -0,0 +1,16 @@
+function soma(x,y){
+    if (typeof x !== 'number' || typeof y !== 'number'){
+        throw new Error('x e y precisam ser números.');
+    }
+
+    return x + y;
+}
+
+
+try{
+    console.log(soma(1,3));
+    console.log(soma(1,'3'));
+}catch(error){
+    //console.log(error);
+    console.log('Ocorreu um erro!');
+}
