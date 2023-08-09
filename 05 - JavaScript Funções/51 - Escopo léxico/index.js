2130e353dc3bef9d1a3fb481732a5b94caa299b7
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-08-09 17:09:50 -0300

    51 - Escopo léxico

---
+++ 05 - JavaScript Funções/51 - Escopo léxico/index.js
@@ -0,0 +1,13 @@
+const nome = 'Natanael';
+
+function falaNome(){
+    const nome = 'Luiz';
+    console.log(nome);
+}
+falaNome();
+
+function usaFalaNome(){
+    const nome = 'Otávio';
+    falaNome();
+}
+usaFalaNome();
