860ed3313fc3093997946dd141b3b1f3af64cd6f
Natanael Marconato dos Santos <nmsantos1@users.noreply.github.com>
2023-08-28 15:07:05 -0300

    56 - Praticando com factory functions

---
+++ 05 - JavaScript Funções/56 - Praticando com factory functions (calculadora)/assets/js/main.js
@@ -0,0 +1,71 @@
+function criaCalc(){
+    return {
+        display: document.querySelector('.display'),
+
+        inicia(){
+            this.clickBtn();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 3){
+                    this.realizaConta();
+                }
+            })
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try{
+                conta = eval(conta);
+
+                if(!conta){
+                    alert('Conta inválida!');
+                    return;
+                }
+                this.display.value = String(conta);
+
+            } catch(e){
+                alert('Conta inválida!');
+                return;
+            }
+        },
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        
+        clickBtn(){
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+            this.display.focus();
+        },
+    };
+}
+
+const claculadora = criaCalc();
+claculadora.inicia();
